fa12982fb7ac9101e29c5fe16a1115535dc02d8f
gotoAbraxas <lty7085@gmail.com>
2023-10-06 12:34:13 +0900

    아이피 다시설정

--- src/network/api.jsx
+++ src/network/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 axios.defaults.baseURL =
-    process.env.NODE_ENV === 'development' ? `http://localhost:8090` : `http://10.128.0.5:8090`
+    process.env.NODE_ENV === 'development' ? `http://localhost:8090` : `http://34.132.223.230:8090`
 
 export const apiNoToken = async (url, method, data) => {
     const body = await axios(
